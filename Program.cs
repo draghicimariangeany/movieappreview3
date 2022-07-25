d437b89c8aa5c7bba2c2893adea81251ab9fe4e5
draghicimariangeany <37182181+draghicimariangeany@users.noreply.github.com>
2022-07-25 14:36:12 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ namespace MvcMovie
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-            .UseUrls("http://localhost:5000")
+            .UseUrls("http://localhost")
                 .UseStartup<Startup>();
     }
 }
